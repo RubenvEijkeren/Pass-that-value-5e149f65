8b74021c2faa0bf05bba9fdc96720a2437fb4998
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-05-15 11:44:14 +0200

    Uitwerking van de exercise

---
+++ value.php
@@ -0,0 +1,19 @@
+<?php
+$onetwo = ['1', '2'];
+$three = '3';
+addItemToArray($onetwo, $three);
+
+function addItemToArray($array, $item){
+    array_push($array, $item);
+    var_dump($array);
+}
+var_dump($onetwo);
+
+addItemToArray2($onetwo, $three);
+
+function addItemToArray2(&$array, $item){
+    array_push($array, $item);
+    var_dump($array);
+}
+var_dump($onetwo);
+?>
